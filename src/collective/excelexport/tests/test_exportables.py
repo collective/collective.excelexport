b270fcb1d58c1383163b3f4db88c04a9b97a9da0
Christophe Boulanger <christophe.boulanger@imio.be>
2019-09-12 15:14:52 +0200

    test ChoiceFieldRenderer render_collection_entry and render_value with unicode

--- src/collective/excelexport/tests/test_exportables.py
+++ src/collective/excelexport/tests/test_exportables.py
@@ -61,12 +61,6 @@ class TestExportables(IntegrationTestCase):
         field.value = 'sgeulette'
         renderer = ChoiceFieldRenderer(field, self.folder, self.portal.REQUEST)
         self.assertEqual(renderer.render_value(renderer.context), 'Stephan Geulette')
-        # render_collection_entry with non unicode vocabulary value
-        devs = SimpleVocabulary([SimpleTerm(value=u'sgeulette', title='Stephan Geulette')])
-        field = schema.Choice(title=u'Test', vocabulary=devs)
-        field.value = 'sgeulette'
-        renderer = ChoiceFieldRenderer(field, self.folder, self.portal.REQUEST)
-        self.assertEqual(renderer.render_collection_entry(renderer.context, 'Stéphan Geulette'), u'Stéphan Geulette')
 
         # with source param
 
@@ -78,6 +72,22 @@ class TestExportables(IntegrationTestCase):
         field.value = 'sgeulette'
         renderer = ChoiceFieldRenderer(field, self.folder, self.portal.REQUEST)
         self.assertEqual(renderer.render_value(renderer.context), 'Stephan Geulette')
+
+    def test_ChoiceFieldRenderer_render_collection_entry_unicode(self):
+        # render_collection_entry with non unicode vocabulary value
+        devs = SimpleVocabulary([SimpleTerm(value=u'sgeulette', title='Stéphan Geulette')])
+        field = schema.Choice(title=u'Test', vocabulary=devs)
+        field.value = 'sgeulette'
+        renderer = ChoiceFieldRenderer(field, self.folder, self.portal.REQUEST)
+        self.assertEqual(renderer.render_collection_entry(renderer.context, field.value), u'Stéphan Geulette')
+
+    def test_ChoiceFieldRenderer_render_value_unicode(self):
+        # render_collection_entry with non unicode vocabulary value
+        devs = SimpleVocabulary([SimpleTerm(value=u'sgeulette', title='Stéphan Geulette')])
+        field = schema.Choice(title=u'Test', vocabulary=devs)
+        field.value = 'sgeulette'
+        renderer = ChoiceFieldRenderer(field, self.folder, self.portal.REQUEST)
+        self.assertEqual(renderer.render_value(renderer.context), u'Stéphan Geulette')
 
     def test_get_exportable_for_fieldname(self):
         member = api.content.create(self.portal, type='member', id='johndoe', languages=['en', 'de'])
