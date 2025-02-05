e9de61fce816ed4d37f7645502f4f9df40c88015
Laurent Lasudry <lasudry@gmail.com>
2025-02-05 09:13:48 +0100

    Add conditional import for Plone 6

--- src/collective/excelexport/exportables/archetypesfields.py
+++ src/collective/excelexport/exportables/archetypesfields.py
@@ -12,7 +12,10 @@ from collective.excelexport.interfaces import IExportable
 from z3c.form.interfaces import NO_VALUE
 from zope.component import adapts
 from zope.component import getMultiAdapter
-from zope.component.interfaces import ComponentLookupError
+try:
+    from zope.interface.interfaces import ComponentLookupError
+except ImportError:
+    from zope.component.interfaces import ComponentLookupError
 from zope.i18n import translate
 from zope.i18nmessageid.message import Message
 from zope.interface import Interface
